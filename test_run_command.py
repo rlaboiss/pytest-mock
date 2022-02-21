fef9ba935bfb5e32c4c0a48642051c49c082f752
Rafael Laboissière <rafael@laboissiere.net>
2022-02-21 19:16:32 +0100

    Add comments for the unit tests

--- test_run_command.py
+++ test_run_command.py
@@ -3,18 +3,29 @@ from unittest.mock import Mock, patch
 from run_command import RunCommand
 
 
+### This unit test exercises the full functionality of the RunCommand class
+### (the command "ls -l /etc/ssh" is effectively run in the system). It must
+### succeed.
 def test_success():
     rc = RunCommand("ls -l /etc/ssh")
     ret = rc.run()
     assert ret == 0
 
 
+### This unit test must fail, unless there is a file or directory names
+### /etc/foo in the system, what is very unlikely.
 def test_failure():
     rc = RunCommand("ls -l /etc/foo")
     ret = rc.run()
     assert ret == 0
 
 
+### This unit test must succeed, even though, like in the unit test
+### test_failure above, it would try to list a non existing file in the
+### system (/etc/foo). This is done by mocking the Popen class used in file
+### run_command.py (through mock.patch) and by mocking the return value of
+### the Popen method (through the __enter__ method of the context manager
+### created by the "with Popen()" call).
 @patch("run_command.Popen")
 def test_patch(popen_mock: Mock):
     popen_mock.return_value.__enter__.return_value.returncode = 0
--- test_write_file.py
+++ test_write_file.py
@@ -3,6 +3,9 @@ from unittest.mock import Mock, patch
 from write_file import WriteFile
 
 
+### This unit test exercises the full functionality of the WriteFile class
+### (the file /tmp/foo is effectively written in the file system). It must
+### succeed.
 def test_success():
     s = "message"
     wf = WriteFile("/tmp/foo")
@@ -10,6 +13,8 @@ def test_success():
     assert ret == len(s)
 
 
+### This unit test must fail, unless it is run with root permissions.
+### Indeed, it tries to write into the /root/foo file.
 def test_failure():
     s = "message"
     wf = WriteFile("/root/foo")
@@ -17,6 +22,12 @@ def test_failure():
     assert ret == len(s)
 
 
+### This unit test must succeed, even though, like in the unit test
+### test_failure above, it would write into a non accessible file
+### (/root/foo). This is done by mocking the open class used in file
+### write_file.py (through mock.patch) and by mocking the return value of
+### the open.write() method (through the __enter__ method of the context
+### manager created by the "with open()" call).
 @patch("write_file.open")
 def test_patch(open_mock: Mock):
     open_mock.return_value.__enter__.return_value.write.return_value = 0
